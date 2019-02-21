335094cabbbbcfd3f647ea04f65549b4408a93cc
Joel Rebello <joel.rebello@booking.com>
2019-02-21 22:40:41 +0100

    fix up test client to set auth header on redirects

--- test.go
+++ test.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 
 	"github.com/bmc-toolbox/redgopher/openapi"
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -24,28 +23,38 @@ func main() {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
+	// declare behaviour on redirects
+	checkRedirect := func(req *http.Request, via []*http.Request) error {
+		if len(via) >= 5 {
+			log.Printf("Too many redirects - %d\n", len(via))
+		}
+		// Sensitive headers are not forwarded by default
+		req.SetBasicAuth(user, pass)
+		return nil
+	}
+
 	cfg := &openapi.Configuration{
 		BasePath:   fmt.Sprintf("http://%s", ip),
-		HTTPClient: &http.Client{Transport: tr},
+		HTTPClient: &http.Client{Transport: tr, CheckRedirect: checkRedirect},
 	}
+
 	client := openapi.NewAPIClient(cfg)
 
 	ctx := context.Background()
 	basicAuth := openapi.BasicAuth{UserName: user, Password: pass}
 	ctx2 := context.WithValue(ctx, openapi.ContextBasicAuth, basicAuth)
 
-	//	sessionParam := openapi.Session{UserName: user, Password: pass }
-	//	sessions, _, err := client.DefaultApi.RedfishV1SessionServiceSessionsPost(ctx2, sessionParam)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
+	s, _, err := client.DefaultApi.RedfishV1SessionServiceSessionsGet(ctx2)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//	spew.Dump(sessions)
+	log.Printf("%+v", s)
 
 	l, _, err := client.DefaultApi.RedfishV1ManagersManagerIdLogServicesGet(ctx2, "1")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	spew.Dump(l)
+	log.Printf("%+v", l)
 }
